7c84ea00ec4c3f32cf9c5ede1f7822fbbf92a8d3
151250084 <151250084@smail.nju.edu.cn>
2017-03-06 21:07:53 +0800

    jiemiansheji

--- Eureka_Quantour/Client/src/main/java/presentation/borderUI/BorderController.java
+++ Eureka_Quantour/Client/src/main/java/presentation/borderUI/BorderController.java
@@ -3,10 +3,30 @@ package presentation.borderUI;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 public class BorderController implements Initializable {
+	@FXML
+	DatePicker beginDatePicker;
+	
+	@FXML
+	DatePicker endDatePicker;
+	
+	@FXML
+	TextField searchTextField;
+	
+	@FXML
+	Button searchButton;
+	
+	@FXML
+	protected void goSearch(ActionEvent e){
+		
+	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
