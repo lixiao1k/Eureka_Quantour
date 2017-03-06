c88138137e0f616973bf3ce0f4e514fae9b8c2b3
151250084 <151250084@smail.nju.edu.cn>
2017-03-06 13:21:06 +0800

    update

--- Eureka_Quantour/Client/src/main/java/presentation/loginController.java
+++
@@ -1,7 +0,0 @@
-package presentation;
-
-public class loginController {
-   public static void main(String[] args) {
-	System.out.println("Hello,world");
-}
-}
---
+++ Eureka_Quantour/Client/src/main/java/presentation/loginUI/LoginController.java
@@ -0,0 +1,16 @@
+package presentation.loginUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class LoginController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
