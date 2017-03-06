edc989de31f021438f778072b0303690b6f22f92
151250067 <151250067@smail.nju.edu.cn>
2017-03-06 15:26:45 +0800

    update

--- Eureka_Quantour/Client/src/main/java/presentation/loginUI/Main.java
+++ Eureka_Quantour/Client/src/main/java/presentation/loginUI/Main.java
@@ -11,7 +11,8 @@ public class Main extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		Parent root = FXMLLoader.load(getClass().getResource("loginUi.fxml"));
+		
+		Parent root = FXMLLoader.load(getClass().getResource("/presentation/loginUI/loginUi.fxml"));
 		Scene scene = new Scene(root);
 	    primaryStage.setScene(scene);
 	    primaryStage.show();
