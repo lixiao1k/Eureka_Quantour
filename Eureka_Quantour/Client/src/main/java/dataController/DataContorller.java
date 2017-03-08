257fd4ea8dfc915b440ba48bf1798a820377d1a8
151250084 <151250084@smail.nju.edu.cn>
2017-03-08 17:12:33 +0800

    add some function to UI

---
+++ Eureka_Quantour/Client/src/main/java/dataController/DataContorller.java
@@ -0,0 +1,35 @@
+package dataController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataContorller {
+    private Map<String,Object> map = new HashMap<>();
+    private static DataContorller instance;
+    public static DataContorller getInstance(){
+    	if(instance==null){
+    		instance = new DataContorller();
+    		return instance;
+    	}else{
+    		return instance;
+    	}
+    }
+    
+    public void put(String name, Object o){
+    	map.put(name, o);
+    }
+    public Object get(String name){
+    	return map.get(name);
+    }
+    public void remove(String name){
+    	map.remove(name);
+    }
+    public void upDate(String name,Object o){
+    	if(map.containsKey(name)){
+    		map.remove(name);
+    		map.put(name, o);
+    	}else{
+    		map.put(name, o);
+    	}
+    }
+}
--- Eureka_Quantour/Client/src/main/java/presentation/borderUI/BorderController.java
+++ Eureka_Quantour/Client/src/main/java/presentation/borderUI/BorderController.java
@@ -1,12 +1,15 @@
 package presentation.borderUI;
 
 import java.net.URL;
+import java.util.Observable;
 import java.util.ResourceBundle;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -15,6 +18,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class BorderController implements Initializable {
 	@FXML
@@ -30,6 +34,14 @@ public class BorderController implements Initializable {
 	
 	@FXML
 	protected void browseKLine(ActionEvent e){
+		ObservableList<Node> nodeList = setInfoAnchorPane.getChildren();
+		for(Node node:nodeList){
+			String name = (String)node.getProperties().get("Name");
+			if(name!=null&&name.contains("HBox")){
+				nodeList.remove(node);
+				break;
+			}
+		}
 		setInfoAnchorPane.getChildren().add(searchSingleStockHBox());
 	}
 	
@@ -40,12 +52,26 @@ public class BorderController implements Initializable {
 	
 	@FXML
 	protected void goCompare(ActionEvent e){
+		ObservableList<Node> nodeList = setInfoAnchorPane.getChildren();
+		for(Node node:nodeList){
+			String name = (String)node.getProperties().get("Name");
+			if(name!=null&&name.contains("HBox")){
+				nodeList.remove(node);
+				break;
+			}
+		}
 		setInfoAnchorPane.getChildren().add(compareHBox());
 	}
 	
 	@FXML
 	protected void browseMarket(ActionEvent e){
 		
+	}
+	
+	@FXML
+	protected void exit(ActionEvent e){
+		Stage stage = (Stage)imageView.getScene().getWindow();
+		stage.close();
 	}
 	
 	private HBox searchSingleStockHBox (){
@@ -65,6 +91,7 @@ public class BorderController implements Initializable {
 		Button searchButton = new Button("搜索");
 		
 		hb.getChildren().addAll(beginLabel,beginDatePicker,endLabel,endDatePicker,blank,stockName,searchButton);
+		hb.getProperties().put("Name","searchSingleStockHBox");
 		return hb;
 	}
 	
@@ -88,7 +115,7 @@ public class BorderController implements Initializable {
 		Button compareButton = new Button("比较");
 		
 		hb.getChildren().addAll(beginLabel,beginDatePicker,endLabel,endDatePicker,blank,stockNameA,stockNameB,compareButton);
-		
+		hb.getProperties().put("Name","compareHBox");
 		return hb;
 	}
 
