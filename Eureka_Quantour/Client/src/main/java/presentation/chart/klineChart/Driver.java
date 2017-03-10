a8a9203d939e54751fcdcdddc7b5dfa413d90b51
151250067 <151250067@smail.nju.edu.cn>
2017-03-10 08:38:20 +0800

    merge

--- Eureka_Quantour/Client/src/main/java/presentation/chart/klineChart/Driver.java
+++ Eureka_Quantour/Client/src/main/java/presentation/chart/klineChart/Driver.java
@@ -1,29 +1,9 @@
 package presentation.chart.klineChart;
-/*
-Copyright 2014 Zoi Capital, LLC
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
- */
-
-
-import java.text.DecimalFormat;
 import java.util.*;
 
 import javafx.application.Application;
-<<<<<<< HEAD
-import static javafx.application.Application.launch;
-=======
->>>>>>> ba3514983e2dd69f921c53e425e144e0ca8acc14
+
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import presentation.chart.barChart.VolumeChart;
