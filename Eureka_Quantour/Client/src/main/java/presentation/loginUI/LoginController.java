fa9a67e002821edf3e396121385aaede49567758
151250084 <151250084@smail.nju.edu.cn>
2017-03-27 11:35:16 +0800

    主面板，补上

--- Eureka_Quantour/Client/src/main/java/presentation/loginUI/LoginController.java
+++ Eureka_Quantour/Client/src/main/java/presentation/loginUI/LoginController.java
@@ -1,16 +1,21 @@
 package presentation.loginUI;
 
 import java.awt.Label;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class LoginController implements Initializable{
 	@FXML
@@ -20,7 +25,7 @@ public class LoginController implements Initializable{
 	TextField passwordTextField;
 	
 	@FXML
-	HBox callBackLabel;
+	HBox callBackHBox;
 	
 	@FXML
 	ImageView logoImageView;
@@ -31,8 +36,12 @@ public class LoginController implements Initializable{
 	}
 	
 	@FXML
-	protected void logIn(ActionEvent e){
-		
+	protected void logIn(ActionEvent e) throws IOException{
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("presentation/mainScreen/MainScreen.fxml"));
+		Scene scene = new Scene(root);
+		Stage stage = new Stage();
+		stage.setScene(scene);
+		stage.show();
 	}
 
 	@Override
---
+++ Eureka_Quantour/Client/src/main/java/presentation/mainScreen/MainScreenController.java
@@ -0,0 +1,75 @@
+package presentation.mainScreen;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
+public class MainScreenController implements Initializable{
+	
+	@FXML
+	DatePicker nowDatePicker;
+	
+	@FXML
+	Button editButton;
+	
+	@FXML
+	Button exitButton;
+	
+	@FXML
+	Label nameLabel;
+	
+	@FXML
+	Button stockSetButton;
+	
+	@FXML
+	Button marketButton;
+	
+	@FXML
+	Button singleStockButton;
+	
+	@FXML
+	Button mineButton;
+	
+	@FXML
+	protected void browseStockSet(ActionEvent e){
+		
+	}
+	
+	@FXML
+	protected void browseMarket(ActionEvent e){
+		
+	}
+	
+	@FXML
+	protected void browseSingleStock(ActionEvent e){
+		
+	}
+	
+	@FXML
+	protected void browseMine(ActionEvent e){
+		
+	}
+	
+	@FXML
+	protected void edit(ActionEvent e){
+		
+	}
+	
+	@FXML
+	protected void exit(ActionEvent e){
+		
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
