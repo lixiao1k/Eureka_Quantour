ccff2241ac3c1b6677dbc24cb12a0f7ff0825a72
151250100 <151250100@smail.nju.edu.cn>
2017-04-14 23:53:23 +0800

    revise

--- Eureka_Quantour/Server/src/main/java/data/datahelperimpl/MainDataBuffer.java
+++ Eureka_Quantour/Server/src/main/java/data/datahelperimpl/MainDataBuffer.java
@@ -1,75 +1,90 @@
 package data.datahelperimpl;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
-import java.util.HashMap;
 
 public class MainDataBuffer {
-	private static int BUFFER_SIZE=1024*1024*2;
-	private MappedByteBuffer[] queue;
-	private HashMap<Integer,Integer> index;
-	private int nextClear;
-	private int[] codequeue;
-	private int size;
+	private static int BUFFER_SIZE=1024*1024*4;
+	private static int[] block={1,2,3,4};
+	private long[] seperate;
 	private byte[] dst_BUFFERSIZE;
-	public StockIndexBuffer(int size){
+	private String path;
+	private MappedByteBuffer mainData;
+	private long size;
+	private int nowpart;
+	private long partSize;
+	public MainDataBuffer(){
+		path="config/resources/mainData";
+		File main=new File(path);
+		size=main.length();
+		partSize=size/4;
 		dst_BUFFERSIZE=new byte[BUFFER_SIZE];
-		this.size=size;
-		nextClear=0;
-		queue=new MappedByteBuffer[size];
-		index=new HashMap<Integer,Integer>();
-		codequeue=new int[size];
+		seperate=new long[5];
+		seperate[0]=0;
+		seperate[1]=partSize;
+		seperate[2]=partSize*2;
+		seperate[3]=partSize*3;
+		seperate[4]=partSize*4;
 	}
-	public void addBuffer(MappedByteBuffer mbb,int code){
-		int removeCode=codequeue[nextClear];
-		index.remove(removeCode);
-		queue[nextClear]=mbb;
-		codequeue[nextClear]=code;
-		index.put(code, nextClear);
-		nextClear++;
-		if(nextClear==size){
-			nextClear=0;
+	public String read(int position,int length){
+		int thispart=0;
+		for(int i=0;i<4;i++){
+			if(seperate[i+1]>position){
+				thispart=block[i];
+				break;
+			}
 		}
-	}
-	public int getIndex(int code){
-		return index.getOrDefault(code, -1);
-	}
-	public MappedByteBuffer getMbbwithIndex(int index){
-		return queue[index];
-	}
-	public MappedByteBuffer getMbb(int code,String path){
-		int index=getIndex(code);
-		if(getIndex(code)==-1){
+		if(nowpart==thispart){
+			if((position+length)>=seperate[thispart+1]){
+				try {
+					byte[] dst=new byte[length];
+					loadStockDateIndex(path,position,length).get(dst);
+					return new String(dst);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return null;
+				}
+			}
+			else{
+				byte[] dst=new byte[length];
+				mainData.position((int) (position-seperate[thispart]));
+				mainData.get(dst);
+				return new String(dst);
+			}
+		}
+		else{
+			mainData=null;
+			System.gc();
 			try {
-				MappedByteBuffer mbb=loadStockDateIndex(path);
-				addBuffer(mbb,code);
-				return mbb;
+				byte[] dst=new byte[length];
+				loadStockDateIndex(path,(int)seperate[thispart],(int)partSize).get(dst);
+				return new String(dst);
 			} catch (IOException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 				return null;
 			}
 		}
-		else{
-			return queue[index];
-		}
 	}
-	private MappedByteBuffer loadStockDateIndex(String path) throws IOException{
+	private MappedByteBuffer loadStockDateIndex(String path,int position,int length) throws IOException{
 		FileInputStream is=new FileInputStream(path);
 		FileChannel fc=is.getChannel();
-		MappedByteBuffer mbb=fc.map(MapMode.READ_ONLY, 0, fc.size());
-//		byte[] tempdst=new byte[(int) (fc.size()%BUFFER_SIZE)];
-//		for(int i=0;i<fc.size();i+=BUFFER_SIZE){
-//			if(fc.size()-i>BUFFER_SIZE){
-//				mbb.get(dst_BUFFERSIZE);
-//			}
-//			else{
-//				mbb.get(tempdst);
-//			}
-//		}
+		mainData=fc.map(MapMode.READ_ONLY, position, length);
+		byte[] tempdst=new byte[(int) (fc.size()%BUFFER_SIZE)];
+		for(int i=0;i<fc.size();i+=BUFFER_SIZE){
+			if(fc.size()-i>BUFFER_SIZE){
+				mainData.get(dst_BUFFERSIZE);
+			}
+			else{
+				mainData.get(tempdst);
+			}
+		}
 		is.close();
-		return mbb;
+		return mainData;
 	}
 }
