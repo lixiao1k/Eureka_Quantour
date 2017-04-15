a5c06b1389be62578e8f47507525c78125bbc0d8
151250084 <151250084@smail.nju.edu.cn>
2017-04-15 18:38:16 +0800

    rmi

--- Eureka_Quantour/Client/src/main/java/logic/service/StockLogicInterface.java
+++ Eureka_Quantour/Client/src/main/java/logic/service/StockLogicInterface.java
@@ -22,14 +22,14 @@ public interface StockLogicInterface extends Remote {
 	 * @return: Iterator<SingleStockInfoVO>
 	 */
 	 List<SingleStockInfoVO> getSingleStockInfoByTime (String stockCode, LocalDate begin, LocalDate end )
-			 throws RemoteException, DateInvalidException, BeginInvalidException, EndInvalidException, NullStockIDException;
+			 throws RemoteException, DateInvalidException, BeginInvalidException, EndInvalidException, NullStockIDException,RemoteException;
 	/**
 	 * @Description: to get all stocks' EMA between "begin" and "end",
 	 *               invoke getSingleStockInfo() in data layer
 	 * @return: Iterator<Double>
 	 */
 	 List<EMAInfoVO> getEMAInfo ( String stockCode, LocalDate begin, LocalDate end )
-			 throws RemoteException, DateInvalidException, BeginInvalidException, EndInvalidException, NullStockIDException, DateOverException;
+			 throws RemoteException, DateInvalidException, BeginInvalidException, EndInvalidException, NullStockIDException, DateOverException,RemoteException;
 	/**
 	 * @Description: to get stock A and B's compared information between "begin" and "end",
 	 *               invoke getSingleStockInfo() in data layer
@@ -46,24 +46,24 @@ public interface StockLogicInterface extends Remote {
 //			throws RemoteException, DateInvalidException, BeginInvalidException, EndInvalidException;
 
 
-	 List<String> getStockSet(String username);
+	 List<String> getStockSet(String username)throws RemoteException;
 
-	 List<SingleStockInfoVO> getStockSetSortedInfo(String stockSetName, LocalDate now,String username);
+	 List<SingleStockInfoVO> getStockSetSortedInfo(String stockSetName, LocalDate now,String username)throws RemoteException;
 
-	 SingleStockInfoVO getStockBasicInfo(String code, LocalDate now) throws NullStockIDException, NullDateException;
+	 SingleStockInfoVO getStockBasicInfo(String code, LocalDate now) throws NullStockIDException, NullDateException,RemoteException;
 
-	 void setStrategy(StrategyConditionVO strategyConditionVO, SaleVO s, LocalDate begin, LocalDate now, String stockSetName,String username);
+	 void setStrategy(StrategyConditionVO strategyConditionVO, SaleVO s, LocalDate begin, LocalDate now, String stockSetName,String username)throws RemoteException;
 
-	 YieldChartDataVO getYieldChartData();
+	 YieldChartDataVO getYieldChartData()throws RemoteException;
 
-	 YieldDistributionHistogramDataVO getYieldDistributionHistogramData();
+	 YieldDistributionHistogramDataVO getYieldDistributionHistogramData()throws RemoteException;
 
-	 void addStockSet(String stockSetName, String username) throws StockSetNameRepeatException;
+	 void addStockSet(String stockSetName, String username) throws StockSetNameRepeatException,RemoteException;
 
-	 void deleteStockSet(String stockSetName, String username);
+	 void deleteStockSet(String stockSetName, String username)throws RemoteException;
 
-	 void addStockToStockSet(String stockName, String stockSetName, String username) throws StockNameRepeatException;
+	 void addStockToStockSet(String stockName, String stockSetName, String username) throws StockNameRepeatException,RemoteException;
 
-	 void deleteStockFromStockSet(String stockName, String stockSetName, String username);
+	 void deleteStockFromStockSet(String stockName, String stockSetName, String username)throws RemoteException;
 
 }
--- Eureka_Quantour/Client/src/main/java/main/Test.java
+++ Eureka_Quantour/Client/src/main/java/main/Test.java
@@ -1,13 +1,20 @@
 package main;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 import dataController.DataContorller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import rmi.RemoteHelper;
 
 public class Test extends Application{
+	private static RemoteHelper rmic;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -19,6 +26,21 @@ public class Test extends Application{
 	}
 	public static void main(String[] args) {
 		DataContorller dataController = DataContorller.getInstance();
+
+		try {
+			rmic = RemoteHelper.getInstance();
+			rmic.setRemote(Naming.lookup("rmi://localhost:8888/DateRemote"));
+			System.out.println("连接服务器成功！");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		launch(args);
 	}
 
