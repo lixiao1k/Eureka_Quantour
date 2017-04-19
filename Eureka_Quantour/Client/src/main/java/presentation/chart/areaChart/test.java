387248c1a7f6ea2c8654f72463d375334c6390a1
151250066 <151250066@smail.nju.edu.cn>
2017-04-20 02:05:24 +0800

    test AreaChart

--- Eureka_Quantour/Client/src/main/java/presentation/chart/areaChart/test.java
+++ Eureka_Quantour/Client/src/main/java/presentation/chart/areaChart/test.java
@@ -1,5 +1,8 @@
 package presentation.chart.areaChart;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
@@ -10,6 +13,7 @@ public class test extends Application{
 	private int[] cycles;
 	private Double[] dataList;
 	private static AreaLineChart areaLineChart;
+	private static DanTengChart danTengChart;
 	
 	private void testAreaLineChart(){
 		cycles = new int[20];
@@ -26,19 +30,41 @@ public class test extends Application{
 		areaLineChart = new AreaLineChart(cycles, dataList, "dafa");
 	}
 	
+	private void testDanTengChart(){
+		List<Double> cycle = new ArrayList<>();
+		cycle.add(0.0);
+		cycle.add(5.0);
+		cycle.add(10.0);
+		cycle.add(15.0);
+		cycle.add(20.0);
+		cycle.add(25.0);
+		cycle.add(30.0);
+		
+		List<Double> datalist = new ArrayList<>();
+		for( int i=0; i<cycle.size(); i++ ){
+			if( i==2 || i==3 || i==4 || i==7 )
+				datalist.add( -Math.random() );
+			else
+				datalist.add( Math.random() );
+		}
+		danTengChart = new DanTengChart(cycle, datalist);
+	}
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		 // TODO Auto-generated method stub
-		 new test().testAreaLineChart();
-		 
+//		 new test().testAreaLineChart();
+		 new test().testDanTengChart();
 		try{
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Birthday Statistics");
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.initOwner(primaryStage);
 		        
-			Scene scene = new Scene(areaLineChart.getchart(334, 200, true));
+//			Scene scene = new Scene(areaLineChart.getchart(334, 200, true));
 //			Scene scene = new Scene(areaLineChart.getchart(334, 200, false));
+//			Scene scene = new Scene(danTengChart.getchart(334, 200, false));
+			Scene scene = new Scene(danTengChart.getchart(334, 200, true));
 			dialogStage.setScene(scene);
 
 			dialogStage.show();
