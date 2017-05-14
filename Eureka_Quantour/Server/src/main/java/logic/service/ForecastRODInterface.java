6e0716c5e9a639bd86564c9747cc791c1701937e
151250066 <151250066@smail.nju.edu.cn>
2017-05-14 16:12:03 +0800

    add predict function

--- Eureka_Quantour/Server/src/main/java/logic/service/ForecastRODInterface.java
+++ Eureka_Quantour/Server/src/main/java/logic/service/ForecastRODInterface.java
@@ -4,10 +4,21 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.time.LocalDate;
 
+import vo.PredictVO;
 import vo.StockRODVO;
 
 public interface ForecastRODInterface extends Remote {
 	
-	public StockRODVO getStockROD( String stockcode, LocalDate begindate, LocalDate enddate) throws RemoteException;
+	public StockRODVO getStockROD( String stockcode, LocalDate begindate, LocalDate enddate ) throws RemoteException;
 	
+	/**
+	 * 
+	 * @Description: TODO
+	 * @author: hzp
+	 * @date: May 14, 2017
+	 * @param: stockcode : the code of stock
+	 * @param: date : the newest time of stock
+	 * @return: PredictVO
+	 */
+	public PredictVO predict( String stockcode, LocalDate date ) throws RemoteException;
 }
