2a7bed5ba1581f8e67bc07d6ffac4c63cc7af863
151250067 <151250067@smail.nju.edu.cn>
2017-06-06 21:42:18 +0800

    乱码

--- Eureka_Quantour/WebAPP/src/main/java/servlet/Login.java
+++ Eureka_Quantour/WebAPP/src/main/java/servlet/Login.java
@@ -5,19 +5,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Created by huihantao on 2017/6/6.
  */
-public class a extends HttpServlet {
+public class Login extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8");
-        System.out.println(req.getParameter("Name"));
-        resp.setCharacterEncoding("UTF-8");
-        resp.setHeader("content-type","text/html;charset=UTF-8");
-        resp.getWriter().println("中文");
-        doGet(req, resp);
+        req.getParameter("Name");
+        req.getParameter("Password");
+
     }
 
     @Override
