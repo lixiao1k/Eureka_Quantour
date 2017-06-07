ba2a9683df27c01b4df4addc5212e421a5a0da12
151250067 <151250067@smail.nju.edu.cn>
2017-06-07 14:37:34 +0800

    初步完成登录跳转

--- Eureka_Quantour/WebAPP/src/main/java/logic/service/ClientLogicInterface.java
+++ Eureka_Quantour/WebAPP/src/main/java/logic/service/ClientLogicInterface.java
@@ -1,4 +1,4 @@
-package service;
+package logic.service;
 
 
 import exception.LogErrorException;
--- Eureka_Quantour/WebAPP/src/main/java/logic/service/ForecastRODInterface.java
+++ Eureka_Quantour/WebAPP/src/main/java/logic/service/ForecastRODInterface.java
@@ -1,4 +1,4 @@
-package service;
+package logic.service;
 
 import vo.PredictVO;
 import vo.StockRODVO;
