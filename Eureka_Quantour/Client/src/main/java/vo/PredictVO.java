3021dfcb7a8c72b3277752bb7cd4848dbc9deaf9
151250066 <151250066@smail.nju.edu.cn>
2017-06-13 13:18:29 +0800

    add zhushi

--- Eureka_Quantour/Client/src/main/java/vo/PredictVO.java
+++ Eureka_Quantour/Client/src/main/java/vo/PredictVO.java
@@ -4,7 +4,9 @@ import java.io.Serializable;
 
 /**
  * 
- * @Description: save stock's prediction
+ * @Description: save stock's prediction and min price
+ * 				 firstMinPrice : 根据90%置信区间计算的价格下限
+ * 				 secondMinPrice : 根据99%置信区间计算的价格下限
  * @author: hzp 
  * @date: May 14, 2017
  */
@@ -16,8 +18,14 @@ public class PredictVO implements Serializable{
 	
 	private double predictROD;
 	
+	/**
+	 * 根据90%置信区间计算的价格下限
+	 */
 	private double firstMinPrice = 0;
 	
+	/**
+	 * 根据99%置信区间计算的价格下限
+	 */
 	private double secondMinPrice = 0;
 	
 	public PredictVO(){
--- Eureka_Quantour/Server/src/main/java/vo/PredictVO.java
+++ Eureka_Quantour/Server/src/main/java/vo/PredictVO.java
@@ -4,7 +4,9 @@ import java.io.Serializable;
 
 /**
  * 
- * @Description: save stock's prediction
+ * @Description: save stock's prediction and min price
+ * 				 firstMinPrice : 根据90%置信区间计算的价格下限
+ * 				 secondMinPrice : 根据99%置信区间计算的价格下限
  * @author: hzp 
  * @date: May 14, 2017
  */
